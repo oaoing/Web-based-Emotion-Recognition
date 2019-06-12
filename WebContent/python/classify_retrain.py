830955161f19e378363150039f0d9d5a256bfda2
oaoing <47515257+oaoing@users.noreply.github.com>
2019-06-12 13:36:12 +0900

    Update classify_retrain.py
    그림과 사진 모델을 분리했습니다.

--- WebContent/python/classify_retrain.py
+++ WebContent/python/classify_retrain.py
@@ -7,7 +7,6 @@ import os
 from keras.utils import to_categorical
 import test
 
-
 labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 def record_feedback(pixel, feedback, label, path):
@@ -21,16 +20,20 @@ def record_feedback(pixel, feedback, label, path):
     f.write(line)
     f.close()
 
-def load_image(path):
-    image = np.asarray([cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)])
-    image = image.reshape(image.shape[0], image.shape[1], image.shape[2], 1)
+def load_image(path, isthatpic):
+    if isthatpic:
+        image = np.asarray([cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)])
+        image = image.reshape(image.shape[0], image.shape[1], image.shape[2], 1)
+    else:
+        image = np.asarray([cv2.imread(path)])
+
     image = image / 255.
 
     return image
 
-def retrain(path, feedback, label, model_path):
+def retrain(path, feedback, label, model_path, model_name):
     epochs = 1
-    batch_size = 100
+    batch_size = 1
 
     x_train = load_image(path)
     pixel = [str(p) for p in x_train[0]]
@@ -47,23 +50,30 @@ def retrain(path, feedback, label, model_path):
     if ori >= new:
         print("model_default:", ori)
         print("tmp:", new)
-        os.popen('del ' + model_path + './tmp.h5')
+        os.system('del ' + model_path + './tmp.h5')
     elif new > ori:
         print("model_default:", ori)
         print("tmp:", new)
-        os.popen('del ' + model_path + '/model_default53.h5')
-        os.popen('ren ' + model_path + './tmp.h5 ' + model_path + '/model_default53.h5')
+        os.system('del ' + model_path + model_name)
+        os.system('ren ' + model_path + './tmp.h5 ' + model_path + model_name)
 
 
-def classify(path, model_path):
-    classify_image = load_image(path)
-    model = load_model(model_path +'/model_default53.h5')
-    test_predictions = np.round(model.predict(classify_image))
-    test_predictions = test_predictions.tolist()
-    print(labels[test_predictions[0].index(1.0)])
+def classify(path, model_path, isthatpic):
+    classify_image = load_image(path, isthatpic)
+    if isthatpic:
+        model = load_model(model_path + '/model_default.h5')
+        test_predictions = np.round(model.predict(classify_image))
+        test_predictions = test_predictions.tolist()
+        print(labels[test_predictions[0].index(1.0)])
+    else:
+        model = load_model(model_path + '/model_default_emoticon.h5')
+        test_predictions = np.round(model.predict(classify_image))
+        test_predictions = test_predictions.tolist()
+        print(labels[test_predictions[0].index(1.0)])
 
 if __name__ =='__main__':   # python classify_retrain.py image_path feed_back label
+    isthatpic = True
     if len(sys.argv) == 3:
         classify(sys.argv[1], sys.argv[2])
     elif len(sys.argv) > 3:
-        retrain(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        retrain(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], '/model_default53.h5')
